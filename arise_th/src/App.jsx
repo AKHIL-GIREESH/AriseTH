9b6c6f2a5b6802211ec474df847eccfed31dd846
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-11-16 05:13:14 +0530

    initial

--- arise_th/src/App.jsx
+++ arise_th/src/App.jsx
@@ -10,25 +10,29 @@ function App() {
     <>
       <h1>Arise Treasure Hunt</h1>
       <p>{word}</p>
-      <button onClick={() => setWord((prev => prev+"Q"))}>Q</button>
-      <button onClick={() => setWord((prev => prev+"W"))}>W</button>
-      <button onClick={() => setWord((prev => prev+"E"))}>E</button>
-      <button onClick={() => setWord((prev => prev+"R"))}>R</button>
-      <button onClick={() => setWord((prev => prev+"T"))}>T</button>
-      <button onClick={() => setWord((prev => prev+"Y"))}>Y</button>
-      <button onClick={() => setWord((prev => prev+"U"))}>U</button>
-      <button onClick={() => setWord((prev => prev+"I"))}>I</button>
-      <button onClick={() => setWord((prev => prev+"O"))}>O</button>
-      <button onClick={() => setWord((prev => prev+"P"))}>P</button>
-      <button onClick={() => setWord((prev => prev+"A"))}>A</button>
-      <button onClick={() => setWord((prev => prev+"S"))}>S</button>
-      <button onClick={() => setWord((prev => prev+"D"))}>D</button>
-      <button onClick={() => setWord((prev => prev+"F"))}>F</button>
-      <button onClick={() => setWord((prev => prev+"G"))}>G</button>
-      <button onClick={() => setWord((prev => prev+"H"))}>H</button>
-      <button onClick={() => setWord((prev => prev+"J"))}>J</button>
-      <button onClick={() => setWord((prev => prev+"K"))}>K</button>
-      <button onClick={() => setWord((prev => prev+"L"))}>L</button>
+      <div>
+        <button onClick={() => setWord((prev => prev+"Q"))}>Q</button>
+        <button onClick={() => setWord((prev => prev+"W"))}>W</button>
+        <button onClick={() => setWord((prev => prev+"E"))}>E</button>
+        <button onClick={() => setWord((prev => prev+"R"))}>R</button>
+        <button onClick={() => setWord((prev => prev+"T"))}>T</button>
+        <button onClick={() => setWord((prev => prev+"Y"))}>Y</button>
+        <button onClick={() => setWord((prev => prev+"U"))}>U</button>
+        <button onClick={() => setWord((prev => prev+"I"))}>I</button>
+        <button onClick={() => setWord((prev => prev+"O"))}>O</button>
+        <button onClick={() => setWord((prev => prev+"P"))}>P</button>
+      </div>
+      <div>
+        <button onClick={() => setWord((prev => prev+"A"))}>A</button>
+        <button onClick={() => setWord((prev => prev+"S"))}>S</button>
+        <button onClick={() => setWord((prev => prev+"D"))}>D</button>
+        <button onClick={() => setWord((prev => prev+"F"))}>F</button>
+        <button onClick={() => setWord((prev => prev+"G"))}>G</button>
+        <button onClick={() => setWord((prev => prev+"H"))}>H</button>
+        <button onClick={() => setWord((prev => prev+"J"))}>J</button>
+        <button onClick={() => setWord((prev => prev+"K"))}>K</button>
+        <button onClick={() => setWord((prev => prev+"L"))}>L</button>
+      </div>
       <button onClick={() => setWord((prev => prev+"Z"))}>Z</button>
       <button onClick={() => setWord((prev => prev+"X"))}>X</button>
       <button onClick={() => setWord((prev => prev+"C"))}>C</button>
